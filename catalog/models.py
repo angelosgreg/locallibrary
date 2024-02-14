525660b8c55f7ed8d0adc94dd9ae8caf36384c0f
Angelos Grigorakis <angelosgreg13@gmail.com>
2024-02-14 15:59:40 +0100

    model catalog

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+
+class Genre(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
+    )
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular genre instance."""
+        return reverse('genre-detail', args=[str(self.id)])
