5ae269b06f90c256fda96c87e1f1e6f33fe8da7a
Angelos Grigorakis <angelosgreg13@gmail.com>
2024-02-16 15:55:15 +0100

    book book instance

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,6 +5,13 @@ admin.site.register(Book)
 # admin.site.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     pass
-admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(Genre)
+# admin.site.register(BookInstance)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
 admin.site.register(Author, AuthorAdmin)
