4a9eb20917432c3005f9d8cda3c354d4771cf958
Angelos Grigorakis <angelosgreg13@gmail.com>
2024-02-28 15:12:11 +0100

    models imports

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 import uuid # Required for unique book instances
