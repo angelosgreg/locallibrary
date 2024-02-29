991353d69419c5ff23c6d1b0b1abb65247a398e7
Angelos Grigorakis <angelosgreg13@gmail.com>
2024-02-29 23:59:34 +0100

    url

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
